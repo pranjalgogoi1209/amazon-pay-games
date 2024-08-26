8c1e46c85d16d46e31fdc14b168739ddc330cc7f
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-08-26 17:59:59 +0530

    update

---
+++ src/assets/jackpotPage/index.js
@@ -0,0 +1,33 @@
+import icon01 from "./icon-01.png";
+import icon02 from "./icon-02.png";
+import icon03 from "./icon-03.png";
+import icon04 from "./icon-04.png";
+import icon05 from "./icon-05.png";
+import icon06 from "./icon-06.png";
+import icon07 from "./icon-07.png";
+import icon08 from "./icon-08.png";
+import icon09 from "./icon-09.png";
+import icon10 from "./icon-10.png";
+import icon11 from "./icon-11.png";
+import icon12 from "./icon-12.png";
+import icon13 from "./icon-13.png";
+import icon14 from "./icon-14.png";
+import icon15 from "./icon-15.png";
+
+export {
+  icon01,
+  icon02,
+  icon03,
+  icon04,
+  icon05,
+  icon06,
+  icon07,
+  icon08,
+  icon09,
+  icon10,
+  icon11,
+  icon12,
+  icon13,
+  icon14,
+  icon15,
+};
---
+++ src/utils/jackpot.js
@@ -0,0 +1,33 @@
+import {
+  icon01,
+  icon02,
+  icon03,
+  icon04,
+  icon05,
+  icon06,
+  icon07,
+  icon08,
+  icon09,
+  icon10,
+  icon11,
+  icon12,
+  icon13,
+} from "./../assets/jackpotPage";
+
+const iconsArr = [
+  icon01,
+  icon02,
+  icon03,
+  icon04,
+  icon05,
+  icon06,
+  icon07,
+  icon08,
+  icon09,
+  icon10,
+  icon11,
+  icon12,
+  icon13,
+];
+
+export default iconsArr;
